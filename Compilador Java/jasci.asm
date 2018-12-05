0e30cba99504e1198d8e39194b4dddac42879a63
Jesús Said Llamas Manriquez <saidllamas@MacBook-Air-de-Jesus.local>
2018-12-04 20:16:05 -0600

    ENTREGADO

--- Compilador Java/jasci.asm
+++ Compilador Java/jasci.asm
@@ -2,7 +2,9 @@
 pila segment para stack 'stack'
 pila ends
 datos segment para public 'data'
-    id0 DB 5 5 34 9 55 468 34 71 34 
+id0 DB 61, ?, 61 DUP( ? )  ; variable de lectura
+id1 DB 61, ?, 61 DUP( ? )  ; variable de lectura
+tmp0 DB "a es menor que b" , 10, 13, 24H
 datos ends
 extra segment para public 'data'
 extra ends
@@ -29,6 +31,93 @@ codigo segment para public 'code'
  MOV DL, 79
  INT 10H
  ; Termina limpiar pantalla
+; inicia lectura de cadena
+       lea  bx, id1
+	push 	bx 
+	call	leercad 
+	add 	sp, 2
+       call	slinea
+       ; fin lectura
+; inicia lectura de cadena
+       lea  bx, id0
+	push 	bx 
+	call	leercad 
+	add 	sp, 2
+       call	slinea
+       ; fin lectura
+;;;;;;;;;;;;;;;;
+	lea bx,id1
+	inc bx
+	mov cl,[bx]
+	mov CH,0
+	PUSH CX
+   
+	CDR0:
+		INC BX
+		SUB[BX],BYTE PTR 30H
+	LOOP CDR0	
+
+	LEA BX,id1+2;AQUI SE GUARDA EL 2
+	MOV AL,[BX]
+	MOV DI,0AH ;NO PUEDE SER DX
+	MOV AH,0; PARA QUITAR EL 09 DE AH, PUESTO EN LA FUNCION 9 DE LA INT 21 
+	POP CX
+	DEC CX
+
+	MYS0:
+		INC BX
+		MUL DI
+		MOV DL,[BX]
+		MOV DH,0
+		ADD AX,DX	
+	LOOP MYS0
+	;; AX tiene el decimal leido
+	;;;;;;;;;;;;;;;;
+
+	MOV BX, AX ; en bx primer numero
+	PUSH BX
+	;;;;;;;;;;;;;;;;
+	lea bx,id0
+	inc bx
+	mov cl,[bx]
+	mov CH,0
+	PUSH CX
+   
+	CDR1:
+		INC BX
+		SUB[BX],BYTE PTR 30H
+	LOOP CDR1
+
+	LEA BX,id0+2;AQUI SE GUARDA EL 2
+	MOV AL,[BX]
+	MOV DI,0AH ;NO PUEDE SER DX
+	MOV AH,0; PARA QUITAR EL 09 DE AH, PUESTO EN LA FUNCION 9 DE LA INT 21 
+	POP CX
+	DEC CX
+
+	MYS1:
+		INC BX
+		MUL DI
+		MOV DL,[BX]
+		MOV DH,0
+		ADD AX,DX	
+	LOOP MYS1
+	;; AX tiene el decimal leido
+	;;;;;;;;;;;;;;;;
+
+	POP BX
+	;;; ejemplo de uso del numero ingresado como comparacion para un if ;;;
+	CMP BX,AX ;
+JL MENOR0
+JMP FIN0
+MENOR0: 
+		; code ;
+; Impresion de cadena
+LEA DX, tmp0
+MOV AH,09
+INT 21H
+		JMP FIN0
+FIN0:
     extern implec:far ; imprimir cadena leida de teclado
     extern impnumde:far ; imprime numero decimal
     extern slinea:far ; salto de linea
